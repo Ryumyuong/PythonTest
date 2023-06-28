690e90f6693147e7b7551a9d1769b6b58e17a33f
Ryumyuong <fbaudwh@naver.com>
2023-06-28 12:24:39 +0900

    5장 raw 사진파일 올리기

--- PythonTest/ch5/Code05-18.py
+++ PythonTest/ch5/Code05-18.py
@@ -2,14 +2,18 @@ inFp = None	# 입력 파일
 inStr = ""		# 읽어온 문자열
 lineNum = 1
 
-inFp = open("C:/Temp/test1.txt", "r", encoding="utf-8" )
-#inFp = open("C:/Temp/data1.txt", "r")
+# inFp = open("C:/Temp/test1.txt", "r", encoding="utf-8" )
+inFp = open("C:/Temp/data2.txt", "r")
 
-while True :
-    inStr = inFp.readline()
-    if inStr == "" :
-        break;
-    print("%d : %s" % (lineNum,inStr), end="")
-    lineNum += 1
+# while True :
+#     inStr = inFp.readline()
+#     if inStr == "" :
+#         break;
+#     print("%d : %s" % (lineNum,inStr), end="")
+#     lineNum += 1
+
+inStr = inFp.readlines()
+for i in inStr :
+    print (i, end="")
 
 inFp.close()

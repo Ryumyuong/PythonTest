80f8dbc4c0106ac152884ba95be0bdd58a81af8a
Ryumyuong <fbaudwh@naver.com>
2023-06-30 16:10:10 +0900

    스프링과 db를 연결시키고 파이썬으로 db의 데이터를 불러와서 다시 데이터프레임으로 변환

---
+++ PythonTest/Project/Gui.py
@@ -0,0 +1,32 @@
+import requests
+import pandas as pd
+
+i = 0
+array = []
+datass = set()
+
+# REST API 엔드포인트 URL
+url = 'http://localhost:8088/users'
+
+# REST API GET 요청
+response = requests.get(url)
+
+# 응답 처리
+if response.status_code == 200:
+    data = response.json()
+    df1 = pd.DataFrame(data)
+    for columns in ['num', 'link', 'title', 'subject', 'company', 'ptime'] :
+        for i in range(len(df1)) :
+            df2 = df1.loc[i,'tableItems']
+            df3 = df2[columns]
+            array = array + [df3]
+        datas = {columns : array}
+        array = []
+        if columns == 'num' :
+            df4 = pd.DataFrame(datas)
+        else :
+            df4[columns] = datas[columns]
+    print(df4)
+            
+else:
+    print('Error:', response.status_code)
